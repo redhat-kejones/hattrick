12bfedd1f9a2a8da0cc0efe25a123c9882cd1df9
Kevin Jones <kevinjones@redhat.com>
2018-03-22 13:40:06 -0400

    RHOSP Overcloud Deploy
    Signed-off-by: Kevin Jones <kevinjones@redhat.com>

---
+++ roles/rhosp_overcloud/files/deploy.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+cd ~
+source ~/stackrc
+
+#	-e /home/stack/templates/firstboot-environment.yaml \
+
+openstack overcloud deploy --templates \
+  -r /home/stack/templates/roles_data.yaml \
+  -e /home/stack/templates/timezone.yaml \
+  -e /home/stack/templates/cloudname.yaml \
+  -e /home/stack/templates/ceilometer.yaml \
+  -e /home/stack/templates/fernet.yaml \
+  -e /usr/share/openstack-tripleo-heat-templates/environments/services/sahara.yaml \
+  -e /usr/share/openstack-tripleo-heat-templates/environments/ceph-radosgw.yaml \
+  -e /usr/share/openstack-tripleo-heat-templates/environments/cinder-backup.yaml \
+  -e /home/stack/templates/storage-environment.yaml \
+  -e /home/stack/templates/network-isolation.yaml \
+  -e /home/stack/templates/network-environment.yaml \
+  -e /home/stack/templates/compute.yaml \
+  -e /home/stack/templates/nested-virt-post-deploy.yaml \
+  --control-flavor control \
+  --compute-flavor compute \
+  --ceph-storage-flavor ceph-storage \
+  --control-scale 1 \
+  --compute-scale 3 \
+  --ceph-storage-scale 0 \
+  --ntp-server time.google.com \
+  --timeout 60
