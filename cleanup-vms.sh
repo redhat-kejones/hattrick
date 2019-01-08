36666a4a74537c38fed2143130ffa8d83b6c7d3a
root <root@kvm.hattrick.lab>
2019-01-08 14:12:06 -0500

    Add Registry and Repo server. Add record_ttl to idm dns tasks

--- cleanup-vms.sh
+++ cleanup-vms.sh
@@ -2,12 +2,14 @@
 
 virsh destroy idm
 virsh destroy repo
+virsh destroy registry
 virsh destroy undercloud
 
 virsh undefine idm
 virsh undefine repo
+virsh undefine registry
 virsh undefine undercloud
 
-rm -rf /tmp/idm /tmp/repo /tmp/undercloud
+rm -rf /tmp/idm /tmp/repo /tmp/undercloud /tmp/registry
 rm -rf /var/lib/libvirt/images/*-os.qcow2
 
